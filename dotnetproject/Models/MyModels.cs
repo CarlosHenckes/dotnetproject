456d304882dce7d25033994627de3940a6a51fa5
Carlos Henckes <henckes@uol.com.br>
2018-10-24 09:47:53 -0300

    initial commit

---
+++ dotnetproject/Models/MyModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dotnetproject.Models
+{
+    public class MyModels
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+    }
+}
